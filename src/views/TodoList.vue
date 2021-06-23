3ea9a7b8152d058169923e93ba6f9646cc1daa81
Takahide Sato <whydoyoustudy@gmail.com>
2021-06-24 00:01:40 +0900

    [fix] button position

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -6,24 +6,40 @@
           <v-col xs="12" sm="8">
             <v-card>
               <v-list-item>
-                    <v-list-item-content>
-                      <v-list-item-title>
-                        <v-text-field v-model="newTodo" id="newTodo" name="newTodo" label="Type your task" @keyup.enter="addTodo" />
-                      </v-list-item-title>
-                      <v-btn
-                        color="orange"
-                        dark
-                        v-bind="attrs"
-                        v-on="on"
-                      >
-                        Register
-                      </v-btn>
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      <v-text-field v-model="newTodo" id="newTodo" name="newTodo" label="Type your task" @keyup.enter="addTodo" />
+                    </v-list-item-title>
                     </v-list-item-content>
               </v-list-item>
             </v-card>
           </v-col>
         </v-row>
       </v-container>
+      <template>
+        <v-container
+          fluid
+          class="pa-0"
+        >
+          <v-row justify="center">
+            <v-col
+              cols="12"
+              sm="6"
+            >
+              <div class="text-center">
+                <div class="my-2">
+                  <v-btn
+                    color="warning"
+                    dark
+                  >
+                  Register
+                  </v-btn>
+                </div>
+              </div>
+            </v-col>
+          </v-row>
+        </v-container>
+      </template>
     </v-main>
   </v-app>
 </template>
