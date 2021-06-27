a4dffaa79006810e76ee6e9faea1a5cd40717cc4
Takahide Sato <whydoyoustudy@gmail.com>
2021-06-27 16:13:46 +0900

    [add] insert sample data into mysql

---
+++ db/mysql/sql/dml.sql
@@ -0,0 +1,5 @@
+INSERT INTO users(username, password) VALUES ("sample","password");
+
+INSERT INTO tasks(user_id,task) VALUES ('1', 'task1');
+
+INSERT INTO sub_tasks(user_id,task_id, sub_task) VALUES ('1', '1', 'sub_task1');
