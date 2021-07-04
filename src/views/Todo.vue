00c950b2c61d9bd283c634f790c1bd96837d2a63
Takahide Sato <whydoyoustudy@gmail.com>
2021-07-04 13:56:53 +0900

    [update] todo actions

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,5 +1,16 @@
 <template>
   <div class='home'>
+    <v-text-field
+      v-model="newTaskTitle"
+      @click:append="addTask"
+      @keyup.enter="addTask"
+      class="pa-3"
+      outlined
+      label="Add Task"
+      append-icon="mdi-plus"
+      hide-details
+      clearable
+    ></v-text-field>
     <v-list
       class="pt-0"
       flat
@@ -8,18 +19,33 @@
         v-for="task in tasks"
         :key="task.id"
       >
-        <v-list-item>
-          <template v-slot:default="{ active, }">
+        <v-list-item
+          @click="doneTask(task.id)"
+          :class="{ 'blue lighten-5' : task.done }"
+        >
+          <template v-slot:default>
             <v-list-item-action>
               <v-checkbox
-                :input-value="active"
+                :input-value="task.done"
                 color="primary"
               ></v-checkbox>
             </v-list-item-action>
 
             <v-list-item-content>
-              <v-list-item-title>{{ task.title }}</v-list-item-title>
+              <v-list-item-title
+                :class="{ '.text-decoration-line-through' : task.done }"
+              >
+              {{ task.title }}
+              </v-list-item-title>
             </v-list-item-content>
+            <v-list-item-action>
+              <v-btn
+                @click.stop="deleteTask(task.id)"
+                icon
+              >
+                <v-icon color="primary lighten-1">mdi-delete</v-icon>
+              </v-btn>
+          </v-list-item-action>
           </template>
         </v-list-item>
         <v-divider></v-divider>
@@ -41,24 +67,44 @@ export default {
       date: new Date().getDate(),
       ord: this.nth(new Date().getDate()),
       year: new Date().getFullYear(),
+      newTaskTitle: '',
       tasks: [
-        {
-          id: 1,
-          title: 'Wake up '
-        },
-        {
-          id: 2,
-          title: 'Get bananas'
-        },
-        {
-          id: 3,
-          title: 'Eat bananas'
-        }
+        // {
+        //   id: 1,
+        //   title: 'Wake up ',
+        //   done: false
+        // },
+        // {
+        //   id: 2,
+        //   title: 'Get bananas',
+        //   done: false
+        // },
+        // {
+        //   id: 3,
+        //   title: 'Eat bananas',
+        //   done: false
+        // }
 
       ],
     }
   },
   methods: {
+    addTask() {
+      let newTask = {
+        id: Date.now(),
+        title: this.newTaskTitle,
+        done: false
+      };
+      this.tasks.push(newTask);
+      this.newTaskTitle = ''
+    },
+    doneTask(id) {
+      let task = this.tasks.filter(task => task.id === id)[0]
+      task.done = !task.done
+    },
+    deleteTask(id) {
+      this.tasks = this.tasks.filter(task => task.id !== id)
+    },
     removeTodo() {
       console.log('REMOVE TODO!')
       this.todos = []
