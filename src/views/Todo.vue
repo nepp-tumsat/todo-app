4c90617e5e769962350ddb58aeefb0bfa9a4abe8
Takahide Sato <whydoyoustudy@gmail.com>
2021-07-04 12:50:59 +0900

    [update] list up tasks

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,6 +1,31 @@
 <template>
-  <div class='home pa-6'>
-    <h1>Todo Page</h1>
+  <div class='home'>
+    <v-list
+      class="pt-0"
+      flat
+    >
+      <div
+        v-for="task in tasks"
+        :key="task.id"
+      >
+        <v-list-item>
+          <template v-slot:default="{ active, }">
+            <v-list-item-action>
+              <v-checkbox
+                :input-value="active"
+                color="primary"
+              ></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ task.title }}</v-list-item-title>
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
+
+    </v-list>
   </div>
 </template>>
 
@@ -16,10 +41,18 @@ export default {
       date: new Date().getDate(),
       ord: this.nth(new Date().getDate()),
       year: new Date().getFullYear(),
-      todos: [
+      tasks: [
         {
-          title: 'Sample',
-          done: false
+          id: 1,
+          title: 'Wake up '
+        },
+        {
+          id: 2,
+          title: 'Get bananas'
+        },
+        {
+          id: 3,
+          title: 'Eat bananas'
         }
 
       ],
