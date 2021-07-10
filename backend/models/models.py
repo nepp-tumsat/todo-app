360dbe8a0f0e24cbca50bfd925c5c78ab9b300b1
banana <hayamatsu8810@gmail.com>
2021-07-10 23:39:21 +0900

    [fix] relationship bug

--- backend/models/models.py
+++ backend/models/models.py
@@ -8,8 +8,8 @@ class User(db.Model):
   username = db.Column(db.String(255), nullable=False, unique=True)
   password = db.Column(db.String(255), nullable=False)
   created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
-  tasks = db.relationship('Task',backref=db.backref('task',lazy=True))
-  sub_tasks = db.relationship('Subtask',backref=db.backref('sub_task',lazy=True))
+  tasks = db.relationship('Task',backref=db.backref('user',lazy=True))
+  sub_tasks = db.relationship('Subtask',backref=db.backref('user',lazy=True))
 
 class Task(db.Model):
   __tablename__ = 'tasks'
@@ -18,7 +18,7 @@ class Task(db.Model):
   created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
   limit_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
   task = db.Column(db.String(255), nullable=True)
-  sub_tasks = db.relationship('Subtask',backref=db.backref('sub_task',lazy=True))
+  sub_tasks = db.relationship('Subtask',backref=db.backref('task',lazy=True))
 
 class Subtask(db.Model):
   __tablename__ = 'sub_tasks'
