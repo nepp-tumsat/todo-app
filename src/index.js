157d3399b23454b8ce78a69e1c9c2b6666adb87b
Takahide Sato <whydoyoustudy@gmail.com>
2021-07-15 01:32:53 +0900

    [add] vuex

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex';
 import vuetify from "./plugins/vuetify.js"
 import router from "./router/router.js"
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
   router,
+  store,
   render: h => h(App), // templateレンダリング
 }).$mount('#app') // htmlにappとしてマウントする
---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    loggedIn: false,
+  },
+  mutations: {
+    increment(state) {
+      state.count++
+    }
+  }
+})
+
+store.commit('increment')
+
+export default store
