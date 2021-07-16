5a38c3f37fee74d6011001ea9fdafca56512461b
Takahide Sato <whydoyoustudy@gmail.com>
2021-07-16 23:16:03 +0900

    [fix] child component loop warning

--- src/components/Dialogs/DeleteTask.vue
+++ src/components/Dialogs/DeleteTask.vue
@@ -1,42 +1,33 @@
 <template>
-  <v-row justify="center">
-    <v-dialog
-      v-model="Open_item"
-      max-width="290"
+    <v-card
+      max-width='400'
     >
-      <v-card>
-        <v-card-title class="text-h5"></v-card-title>
-        <v-card-text>
-          本当に削除してよろしいですか？
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            color="green darken-1"
-            text
-            @click="$emit('close')"
-          >
-            キャンセル
-          </v-btn>
-          <v-btn
-            color="green darken-1"
-            text
-            @click="$emit('close')"
-          >
-            削除
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </v-row>
+      <v-card-title></v-card-title>
+      <v-card-text>
+        本当に削除してよろしいですか？
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="green darken-1"
+          text
+          @click="$emit('close')"
+        >
+          キャンセル
+        </v-btn>
+        <v-btn
+          color="green darken-1"
+          textx
+          @click="$emit('close')"
+        >
+          削除
+        </v-btn>
+      </v-card-actions>
+    </v-card>
 </template>
 
 <script>
 export default {
   name: 'DeleteTask',
-  props: {
-    Open_item: String,
-    required: true
-  }
 }
 </script>
