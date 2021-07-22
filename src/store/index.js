ecb3986b66d06c4b71979e90f45171761f8950da
Takahide Sato <whydoyoustudy@gmail.com>
2021-07-22 18:44:41 +0900

    [add] store user_id

--- src/store/index.js
+++ src/store/index.js
@@ -7,13 +7,16 @@ const store = new Vuex.Store({
   state: {
     loggedIn: false,
     token: "",
+    user_id: "",
   },
   mutations: {
-    login(state) {
+    login(state, user_id) {
       state.loggedIn = true;
+      state.user_id = user_id;
     },
-    logout(state) {
+    logout(state, user_id) {
       state.loggedIn = false;
+      state.user_id = user_id;
     },
   },
 });
