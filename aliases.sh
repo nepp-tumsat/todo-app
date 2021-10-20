776fa646229f52ff49b8ec10311cb10d27415048
Takahide Sato <whydoyoustudy@gmail.com>
2021-10-21 02:01:20 +0900

    [fix] aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,5 @@
 alias todo-back='docker-compose up --build backend'
-alias todo-front='docker-compose up --build front'
+alias todo-front='docker-compose up --build fronted'
 alias todo-front-bash='docker exec -it todo_front bash'
 alias todo-back-bash='docker exec -it todo_back bash'
 
